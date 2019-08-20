7ba1173127fd3ad9ee0d59505a3c43b4d4cc839c
shujianbo <shujianbo@itiger.com>
2019-08-20 21:20:23 +0800

    build(package): 新增readme

---
+++ README.md
@@ -0,0 +1,12 @@
+### create-mvvm-element-app
+
+#### Usage
+
+```bash
+$ npm install -g create-mvvm-element-app
+$ create-mvvm-element-app
+```
+
+效果如下：
+
+![](20190820211555.jpg)
---
+++ wlxm/src/index.js
@@ -0,0 +1,30 @@
+import mvvm, { register } from 'mvvm-element';
+import HelloWorld from './components/HelloWorld.js';
+
+register('wlxm-hello-world', HelloWorld);
+
+mvvm({
+  el: document.querySelector('#app'),
+  data: {
+    time: new Date().toLocaleTimeString()
+  },
+  // setup will only be executed once
+  setup(ctx) {
+    updateClock(ctx);
+
+    return {
+      // event handler
+      handleClick() {
+        // do sth
+      }
+    };
+  }
+});
+
+function updateClock(state) {
+  setTimeout(() => {
+    state.time = new Date().toLocaleTimeString();
+
+    updateClock(state);
+  }, 1000);
+}
